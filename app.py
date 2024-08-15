4655a8a53eee66cee5246a78213ee9b763ca9a5b
Your Name <mimi020001@gmail.com>
2024-08-15 20:32:32 +0200

    inicjalizacja strony

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def countdown():
-    target_date = datetime(2024, 12, 31)  # Ustaw tutaj datę docelową
+    target_date = datetime(2024, 8, 16,12,00,00,)  # Ustaw tutaj datę docelową
     current_date = datetime.now()
     time_remaining = target_date - current_date
     return render_template('countdown.html', time_remaining=time_remaining)
