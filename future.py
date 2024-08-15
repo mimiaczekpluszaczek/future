95331dd82d28e72f0e3d29c1e0db70bbd6c456b7
Your Name <mimi020001@gmail.com>
2024-08-15 20:45:30 +0200

    dodanie zamrazacza

--- future.py
+++ future.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 from datetime import datetime
 
 future = Flask(__name__)
+freezer = Freezer(future)
 
 @future.route('/')
-@future.route('/index')
 def countdown():
-    target_date = datetime(2024, 8, 16,12,00,00)
+    target_date = datetime(2024, 8, 16,12,8)
     current_date = datetime.now()
     time_remaining = target_date - current_date
     return render_template('countdown.html', time_remaining=time_remaining)
 
 if __name__ == '__main__':
+    freezer.freeze()
     future.run(debug=True)
--- struktura.md
+++ struktura.md
@@ -1,4 +1,11 @@
 ```
 ├── README.md
+├── future.py
+├── netflify.toml
+├── static
+│   └── style.css
+├── struktura.md
+├── templates
+│   └── countdown.html
 └── tree.py
 ```
