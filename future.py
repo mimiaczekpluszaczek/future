627673e1499ff59bc89976e89d1ac80ee3fcabf3
Your Name <mimi020001@gmail.com>
2024-08-15 20:42:04 +0200

    drobna zmiana daty

--- future.py
+++ future.py
@@ -4,8 +4,9 @@ from datetime import datetime
 future = Flask(__name__)
 
 @future.route('/')
+@future.route('/index')
 def countdown():
-    target_date = datetime(2024, 8, 16,12,00,00,)  # Ustaw tutaj datę docelową
+    target_date = datetime(2024, 8, 16,12,00,00)
     current_date = datetime.now()
     time_remaining = target_date - current_date
     return render_template('countdown.html', time_remaining=time_remaining)
